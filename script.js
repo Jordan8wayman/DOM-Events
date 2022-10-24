41385a5b91d069444597d39358196f93390bc8af
jordan8wayman <jordanwayman1998@gmail.co.uk>
2022-10-24 09:35:24 +0100

    DOM EVENTS EXCERSISE 1
    1.If you click on the list item, it toggles the .done class on and off.
    
    i did this by creating a for loop to add event listeners to each li
    item and call my liClick function. the liClick function then toggles
    the done class.
    
    It took me longer than i would like to admit to figure this out and alot
    of research outside of the course curriculum to be able to figure this
    out i also had to research the 'this' keyword which is taught further
    along in the course however i think i understand it now and it will
    help me in the future as it has helped me to exercise my reasearching
    skills.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var li = document.querySelector("li")
 
 function inputLength() {
 	return input.value.length;
@@ -28,3 +29,19 @@ function addListAfterKeypress(event) {
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+
+// using query selector all to select all li elements.
+var items = document.querySelectorAll("li");
+
+// using a for loop to iterate through each of the li elements and add an event listener to them and
+// then call the liClick function.
+for (var i=0; i < items.length; i++) {
+	items[i].addEventListener("click", liClick);
+}
+
+// create the liClick function this function uses the this keyword to target items[i]. As items[i] is
+// what called the function. this means it will toggle done on whicheve li element called it.
+function liClick() {
+	this.classList.toggle("done");
+}
