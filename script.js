de3e813cd838941fda583c937919f7c07c3f4fea
jordan8wayman <jordanwayman1998@gmail.co.uk>
2022-11-02 15:02:26 +0000

    FINISHED DOM EVENTS TASk
    1) If you click on the list item, it toggles the .done class on and off.
    
    2) Add buttons next to each list item to delete the item when clicked on
    its corresponding delete button.
    
    3) BONUS: When adding a new list item, it automatically adds the delete
    button next to it (hint: be sure to check if new items are clickable
    too!)
    
    This took me way to long to figure out and not sure if i would be able
    to do it again. i had to search through overs code on stack overflow and
    and modify things to complete the task. took me a few days to get my
    head around it but has definatly improved my reasearch skills. my
    original code to complete the first task had to be completely removed
    had to redo the code with the intention of completeing the next tasks
    rather than just adding to it.

--- script.js
+++ script.js
@@ -1,47 +1,72 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
-var li = document.querySelector("li")
+var button = document.getElementById('enter');
+var input = document.getElementById('userinput');
+var ul = document.querySelector('ul');
+var li = document.getElementsByTagName('li')
 
-function inputLength() {
+// adding event listeners yo input and button for new elements.
+button.addEventListener('click', Onclick);
+input.addEventListener('keypress', addElement);
+
+
+liEvent();
+buttonListElement();
+
+function checkInputlength(){
 	return input.value.length;
 }
 
-function createListElement() {
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
+// creating new list elements and addin delete buttons.
+function createListElement(){
+
+		var li = document.createElement("li");
+		var button = document.createElement('button');
+		li.appendChild(document.createTextNode(input.value));
+		ul.appendChild(li);
+		li.appendChild(button)
+		button.innerHTML = "delete";
+		input.value = "";
+
+		liEvent();
+		buttonListElement();
 }
 
-function addListAfterClick() {
-	if (inputLength() > 0) {
+
+// function to check input length and create list Element when clicked.
+function Onclick(){
+
+	if(checkInputlength() > 0){
+		createListElement();
+	}
+
+}
+
+// function to add on keypress.
+function addElement(){
+
+	if(checkInputlength() > 0 && event.keyCode === 13){
 		createListElement();
 	}
 }
-
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
+// function to add event listener to the list elements.
+function liEvent(){
+	for( i=0; i<li.length; i++){
+	li[i].addEventListener('click', changeClass)
 	}
 }
-
-button.addEventListener("click", addListAfterClick);
-
-input.addEventListener("keypress", addListAfterKeypress);
-
-
-// using query selector all to select all li elements.
-var items = document.querySelectorAll("li");
-
-// using a for loop to iterate through each of the li elements and add an event listener to them and
-// then call the liClick function.
-for (var i=0; i < items.length; i++) {
-	items[i].addEventListener("click", liClick);
+// function to toggle the done class of the list element on and off.
+function changeClass(){
+	this.classList.toggle('done');
 }
-
-// create the liClick function this function uses the this keyword to target items[i]. As items[i] is
-// what called the function. this means it will toggle done on whicheve li element called it.
-function liClick() {
-	this.classList.toggle("done");
+// function to add event listeners to the buttons on the list elements.
+function buttonListElement(){
+	var button = document.querySelectorAll('li button');
+	for( i=0; i<button.length; i++){
+	button[i].addEventListener('click', clearElement)
+	}
+}
+// function to delete the list elements when the delete button is clicked.
+function clearElement(){
+	for(var i=0; i<li.length; i++){
+		this.parentNode.remove()
+	}
 }
